1b2be59ab5d48706813d6d7f330c2586c805f7e8
vijay <vijay.barfa@techlect.com>
2012-10-09 13:49:01 +0530

    excel reader

---
+++ example.php
@@ -0,0 +1,41 @@
+<?php
+ini_set("display_errors",1);
+ini_set('memory_limit', '200M');
+error_reporting(E_ALL);
+require_once 'excel_reader2.php';
+$xls = new Spreadsheet_Excel_Reader("Product_spreadsheet_example.xls");
+?>
+<html>
+<head>
+
+<style>
+div { display:none; color:#aaa; }
+</style>
+<script>
+function toggle(state) {
+	var divs = document.getElementsByTagName('div');
+	for (var i=0; i<divs.length; i++) {
+		divs[i].style.display = (state)?'inline':'none';
+	}
+}
+</script>
+</head>
+<body>
+
+Display formatting information: <input type="checkbox" onClick="toggle(this.checked)">
+<br><br>
+
+<table border="1">
+<? for ($row=1;$row<=$xls->rowcount();$row++) { ?>
+	<tr>
+	<? for ($col=1;$col<=$xls->colcount();$col++) {	?>
+		<td><?= $xls->val($row,$col) ?>&nbsp;
+		<div>[Format=<?=$xls->format($row,$col)?>,FormatIndex=<?=$xls->formatIndex($row,$col)?>]</div></td>
+	<? } ?>
+	</tr>
+<? } ?>
+</table>
+
+Note: There seems to be some problem with translating times (and dates with times) correctly.
+</body>
+</html>
